dd620a2efbc6c5985847b83f41ecc580a9633450
Marc Kohaupt <debuglevel@gmail.com>
2025-01-14 14:39:08 +0100

    initial

---
+++ convert.py
@@ -0,0 +1,48 @@
+import os
+import glob
+import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+# Directory containing the XML files
+directory = '.'
+
+# Dictionary to store the data
+data = {}
+
+print("Reading XML files from directory:", directory)
+# Read and parse XML files
+for file in glob.glob(os.path.join(directory, '2025-*')):
+    print("Reading file:", file)
+    tree = ET.parse(file)
+    root = tree.getroot()
+
+    # Extract datetime from the filename
+    filename = os.path.basename(file)
+    file_datetime = datetime.strptime(filename, '2025-%m-%d %H-%M')
+
+    # Extract keywords and votes
+    for keyword in root.findall('.//keyword'):
+        print("Keyword:", keyword.find('name').text.strip())
+        name = keyword.find('name').text.strip()
+        votes = int(keyword.find('stimmen').text.strip())
+
+        if name not in data:
+            data[name] = []
+        data[name].append((file_datetime, votes))
+
+# Plot the data
+print("Plotting the data")
+plt.figure(figsize=(10, 6))
+
+for name, values in data.items():
+    values.sort()  # Sort by datetime
+    dates, votes = zip(*values)
+    plt.plot(dates, votes, label=name)
+
+plt.xlabel('Date and Time')
+plt.ylabel('Votes')
+plt.title('Time Series of Votes for Each Keyword')
+plt.legend()
+plt.grid(True)
+plt.show()
